cd634d40251216632211e690ebe623318b439f8a
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2021-01-26 22:45:05 +0530

    Update cmd.py

--- bot/plugins/cmd/cmd.py
+++ bot/plugins/cmd/cmd.py
@@ -11,7 +11,7 @@ from bot import msg
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{msg.start}{msg.source}",
+        text=f"Hey {message.from_user.first_name},{msg.start}",
         reply_to_message_id=message.message_id,
         parse_mode = "html"
     )
@@ -19,7 +19,7 @@ async def start(client, message):
 async def help(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{msg.help}{msg.source}",
+        text=f"Hey {message.from_user.first_name},{msg.help}",
         reply_to_message_id=message.message_id,
         parse_mode = "html"
     )
